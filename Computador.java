fe9c202f79c3e48f0e3685506e1f01b3fa962ed7
carolesj <caroline.jesuino.silva@usp.br>
2017-06-16 09:14:46 -0300

    Início da implementação da IA

---
+++ Computador.java
@@ -0,0 +1,37 @@
+/**
+ * Created by caroles on 13/06/2017.
+ */
+public class Computador {
+    Casinha[] j1;
+    Casinha[] j2;
+    Kalah k1;
+    Kalah k2;
+
+    public Computador(){
+
+    }
+
+    void Jogada(Tabuleiro jogo){
+
+    }
+
+    void escolheCasinha(Tabuleiro jogo){
+        int K = checaKalah(jogo.j2);
+        for(int i=0; i<6; i++){
+            if (jogo.j2[i].nFeijoes == 0 && jogo.j1[7-i].nFeijoes != 0){
+                checaJogada(jogo.j2);
+            }
+            else if(jogo.j1[i].nFeijoes == 0 && jogo.j2[7-i]. nFeijoes != 0){
+                checaJogada(jogo.j2);
+            }
+            else if(K != -1){
+                int F = jogo.j2[K].nFeijoes;
+                Jogada(jogo, K);
+            }
+            else{
+                int max = checaMaxOponente(jogo.j1);
+            }
+
+        }
+    }
+}
--- Jogadores.java
+++ Jogadores.java
@@ -4,62 +4,6 @@ import java.io.IOException;
  * Created by caroles on 30/05/2017.
  */
 public class Jogadores {
-	
-    /*public void jogada (int jogador, Casinha[] cJogador, Kalah kJogador,
-                       Casinha[] oponente, int casinha) throws IOException {
-        int quantidade = cJogador[casinha].nFeijoes;
-        boolean flagOponente = false;
-        boolean kalah = false;
-        int fei;
-        // checa se a casinha final vai estar vazia
-        
-    
-        for (int i = 0; i < quantidade; i++) {
-        	
-        	if(i == quantidade-1){
-        		if(!flagOponente && cJogador[casinha].nFeijoes == 0){
-        			fei = oponente[5-casinha].nFeijoes;
-        			oponente[5-casinha].nFeijoes = 0;
-        			kJogador.nFeijoes += fei;
-        		}
-        		else if(flagOponente && oponente[casinha].nFeijoes == 0){
-        			fei = cJogador[5-casinha].nFeijoes;
-        			cJogador[5-casinha].nFeijoes = 0;
-        			kJogador.nFeijoes += fei;
-        		
-        		}
-        	}
 
-        	else if (casinha == 6 && !flagOponente) {
-                casinha = 0;
-                kJogador.nFeijoes++;
-                if(i+1 < quantidade) flagOponente = true;
-                kalah = true;
-
-            }
-            else if(casinha == 6){
-                casinha = 0;
-                flagOponente = false;
-            }
-            else if(!flagOponente) {
-                cJogador[casinha].nFeijoes++;
-                casinha++;
-            }
-            else if(flagOponente){
-                oponente[casinha].nFeijoes++;
-                casinha++;
-            }
-            
-        }
-        if (kalah) { // joga de novo
-        	System.out.println("Sua vez novamente, em que casinha você deseja comecar?");
-        	casinha = EntradaTeclado.leInt();
-        	
-        	jogada (jogador, cJogador, kJogador, oponente, casinha);
-        	
-        	
-        	
-        }
-   
-    }*/
+    public Jogadores()
 }
